26ac3bff12213bf7d4990bfd1d6449b42ac3777a
erwan2212 <erwan2212@gmail.com>
2019-07-24 13:46:27 +0200

    sync

--- Samples/Mount/mount.dpr
+++ Samples/Mount/mount.dpr
@@ -786,7 +786,8 @@ end;
 procedure ShowUsage();
 begin
     Write(ErrOutput, escape_replace('progam.exe\n' +
-    '  /r NfsUrl (ex. /r nfs://server/export/)\t\t NFS export to map.\n' +
+    '  /x proxy (ex. /x myproxy.dll.\n' +
+    '  /r resource to be mounted (ex. /r nfs://server/export/)\t\t NFS export to map.\n' +
     '  /l MountPoint (ex. /l m)\t\t\t Mount point. Can be M:\\ (drive letter) or empty NTFS folder C:\\mount\\dokan .\n' +
     '  /t ThreadCount (ex. /t 5)\t\t\t Number of threads to be used internally by Dokan library.\n\t\t\t\t\t\t More threads will handle more event at the same time.\n' +
     '  /d (enable debug output)\t\t\t Enable debug output to an attached debugger.\n' +
@@ -798,8 +799,8 @@ begin
     '  /c (mount for current session only)\t\t Device only visible for current user session.\n' +
     '  /u (UNC provider name ex. \\localhost\\myfs)\t UNC name used for network volume.\n' +
     //'  /p (Impersonate Caller User)\t\t\t Impersonate Caller User when getting the handle in CreateFile for operations.\n\t\t\t\t\t\t This option requires administrator right to work properly.\n' +
-    //'  /a Allocation unit size (ex. /a 512)\t\t Allocation Unit Size of the volume. This will behave on the disk file size.\n' +
-    //'  /k Sector size (ex. /k 512)\t\t\t Sector Size of the volume. This will behave on the disk file size.\n' +
+    '  /a Allocation unit size (ex. /a 512)\t\t Allocation Unit Size of the volume. This will behave on the disk file size.\n' +
+    '  /k Sector size (ex. /k 512)\t\t\t Sector Size of the volume. This will behave on the disk file size.\n' +
     '  /f User mode Lock\t\t\t\t Enable Lockfile/Unlockfile operations. Otherwise Dokan will take care of it.\n' +
     '  /i (Timeout in Milliseconds ex. /i 30000)\t Timeout until a running operation is aborted and the device is unmounted.\n\n' +
     'Examples:\n' +
@@ -1057,7 +1058,6 @@ begin
   //dokanOperations^.FindStreams := onFindStreams;
   dokanOperations^.Mounted := onMounted;
 
-  //_nfsmount(WideCharToString(RootDirectory) );
   //create filesyste ressources
   if _mount(WideCharToString(RootDirectory))=false then
     begin

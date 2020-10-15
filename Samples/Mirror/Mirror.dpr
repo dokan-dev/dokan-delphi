9741d3d74aca807594e4a868602a7ddc20670caf
Sven Harazim <sven.harazim@landrix.de>
2020-10-15 20:22:02 +0200

    dokan.h header translation 1.4.0.1000

--- Samples/Mirror/Mirror.dpr
+++ Samples/Mirror/Mirror.dpr
@@ -1,32 +1,27 @@
-// Release 1.2.0.1000
-// source: dokany/samples/dokan_mirror/mirror.c
-// commit: f6de99b914b8f858acf940073ae8836eb476de7f
-
 (*
+  Dokan API wrapper for Delphi based on Release 1.4.0.1000
+  https://github.com/dokan-dev/dokany/releases/tag/v1.4.0.1000
+  Copyright (C) 2019 - 2020 Sven Harazim
+
   Dokan : user-mode file system library for Windows
 
-  Copyright (C) 2015 - 2018 Adrien J. <liryna.stark@gmail.com> and Maxime C. <maxime@islog.com>
+  Copyright (C) 2015 - 2019 Adrien J. <liryna.stark@gmail.com> and Maxime C. <maxime@islog.com>
+  Copyright (C) 2020 Google, Inc.
   Copyright (C) 2007 - 2011 Hiroki Asakawa <info@dokan-dev.net>
 
-  https://dokan-dev.github.io/
+  http://dokan-dev.github.io
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation; either version 3 of the License, or (at your option) any
+  later version.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+  You should have received a copy of the GNU Lesser General Public License along
+  with this program. If not, see <http://www.gnu.org/licenses/>.
 *)
 
 program Mirror;
